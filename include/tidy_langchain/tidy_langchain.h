5ac2a066dc8096d3feae3a1503a53d7e914743d4
栀染 <imbue2025@outlook.com>
2026-03-22 12:02:37 +0000

    feat: add umbrella header for public API

---
+++ include/tidy_langchain/tidy_langchain.h
@@ -0,0 +1,26 @@
+#ifndef TIDY_LANGCHAIN_H
+#define TIDY_LANGCHAIN_H
+
+#include "tlc_config.h"
+#include "tlc_types.h"
+#include "tlc_status.h"
+#include "tlc_runtime.h"
+#include "tlc_string.h"
+#include "tlc_json.h"
+#include "tlc_message.h"
+#include "tlc_document.h"
+#include "tlc_memory.h"
+#include "tlc_prompt.h"
+#include "tlc_parser.h"
+#include "tlc_callbacks.h"
+#include "tlc_transport.h"
+#include "tlc_model.h"
+#include "tlc_embeddings.h"
+#include "tlc_vectorstore.h"
+#include "tlc_retriever.h"
+#include "tlc_tool.h"
+#include "tlc_runnable.h"
+#include "tlc_chain.h"
+#include "tlc_agent.h"
+
+#endif
