182ff2d1e3a87b293930c304843ceb41314215dc
栀染 <imbue2025@outlook.com>
2026-03-22 11:55:06 +0000

    feat: add output parser abstraction

---
+++ include/tidy_langchain/tlc_parser.h
@@ -0,0 +1,36 @@
+#ifndef TLC_PARSER_H
+#define TLC_PARSER_H
+
+#include "tlc_status.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct tlc_output_parser_s tlc_output_parser_t;
+typedef tlc_status_t (*tlc_output_parse_fn)(tlc_output_parser_t* parser,
+                                            const char* input,
+                                            char* output,
+                                            size_t output_size);
+
+struct tlc_output_parser_s {
+    char name[TLC_NAME_MAX];
+    void* ctx;
+    tlc_output_parse_fn parse;
+};
+
+tlc_status_t tlc_text_output_parser(tlc_output_parser_t* parser,
+                                    const char* input,
+                                    char* output,
+                                    size_t output_size);
+
+tlc_status_t tlc_json_field_output_parser(tlc_output_parser_t* parser,
+                                          const char* input,
+                                          char* output,
+                                          size_t output_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
